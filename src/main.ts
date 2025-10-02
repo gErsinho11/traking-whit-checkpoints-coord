94d491a44577d132398791370c2706acbb483162
gErsinho11 <ingeniero.gersinho@gmail.com>
2025-10-02 01:15:39 -0500

    repo-prueba-coord

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, RequestMethod } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1', { 
-    exclude: [{ path: 'health', method: RequestMethod.GET }] });
+    exclude: [{ path: 'health', method: RequestMethod.GET }] 
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   await app.listen(process.env.PORT || 3000);
   // eslint-disable-next-line no-console
