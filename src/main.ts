a8caa631aa169aaa46082f54b21f109902e32135
gErsinho11 <ingeniero.gersinho@gmail.com>
2025-10-02 02:43:01 -0500

    repo-prueba-ct

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import { ValidationPipe, RequestMethod } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api/v1', { exclude: [{ path: 'health', method: RequestMethod.GET }] });
+  app.setGlobalPrefix('api/v1', { exclude: [{ path: 'healthz', method: RequestMethod.GET }] });
+
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   await app.listen(process.env.PORT || 3000);
